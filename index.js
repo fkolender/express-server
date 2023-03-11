0362e8fc5831b5730bf9c855f5762f3ed9d5d948
fkolender <hedrite@hotmail.com>
2023-03-11 15:41:10 -0300

    New access points in routing

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {        
-    res.sendFile('index.html', {root: __dirname});      
-                                                        
-});
+app.get('/', function (req, res) {
+    res.send('Welcome to my little Express server.')
+  })
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request at /')
+  })
+
+app.get('/user', function (req, res) {
+    res.send('Got a GET request at /user')
+  })
+
+app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user')
+  })
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 }); 
-
-app.get
